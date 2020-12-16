de1de0d527c9f114496b101c51b7b55ae8b4d40d
onshero <1064610021@qq.com>
2020-12-16 16:40:54 +0800

    2020.12.16 16:40
    添加了商家和商品的增删改查

---
+++ 后端/商家/ser/src/main/java/cn/edu/shu/xj/ser/mapper/GoodsMapper.java
@@ -0,0 +1,9 @@
+package cn.edu.shu.xj.ser.mapper;
+
+import cn.edu.shu.xj.ser.entity.Goods;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface GoodsMapper extends BaseMapper<Goods> {
+}
